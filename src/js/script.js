0eedc6e172642bf24bbb5cd4911c9f523a027021
wesolypl <wesolowski.patryk@zhr.pl>
2019-07-03 14:09:11 +0200

    xd

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
-console.log("przykład");
+const hamburger = document.querySelector(".nav__button");
+const menu = document.querySelector(".nav__menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("nav__button--open");
+  menu.classList.toggle("nav__menu--hidden");
+});
